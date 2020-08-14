dca51b77542cde19676319b5f53ec9d9a131ba71
DoyonKim <bluesky13a@gmail.com>
2020-08-14 15:38:03 +0900

    Add random fortune lib

--- meadowlark.js
+++ meadowlark.js
@@ -1,20 +1,24 @@
-var express = require('express');
-
+var express = require('express');//./가 없는 경우 node_module에서 찾는다.
+var fortune = require('./lib/fortune.js');//./를 붙이는 경우 모듈을 node_modules 디렉토리에서 찾지 않는다.
 var app = express();
 
 //핸들바 뷰 엔진 설정
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
 app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
 
 app.set('port',process.env.PORT || 3000);
+
+//Static 미들웨어-정적 자원을 전송하는 역할
+app.use(express.static(__dirname + '/public'));
 
 app.get('/',function(req,res){
     res.render('home');
 });
 
 app.get('/about',function(req,res){
-    res.render('about');
+    res.render('about',{fortune: fortune.getFortune()});
 });
 
 //커스텀 404 페이지
